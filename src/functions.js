020b508bfc5896611aa02615d697703e3dc93772
weverdev47 <wevertonsantosdev47@gmail.com>
2026-01-06 10:27:27 -0300

    Create initial project structure with functions
    Initial project structure with basic functions.

---
+++ src/functions.js
@@ -0,0 +1,21 @@
+// Requisito 1
+function sum(a, b) {
+
+}
+
+// Requisito 2
+function isEven(number) {
+
+}
+
+// Requisito 3
+function maxNumber(numbers) {
+
+}
+
+module.exports = {
+  sum,
+  isEven,
+  maxNumber,
+};
+Cria estrutura inicial do projeto
